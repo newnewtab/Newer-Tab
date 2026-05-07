541a452ed5828e689aeb9a4bca135cb9969c335a
Cling.Py <154641389+newnewtab@users.noreply.github.com>
2026-05-06 23:32:42 -0400

    Replace hardcoded API key with environment variable

--- firebase-presence.js
+++ firebase-presence.js
@@ -12,7 +12,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAk9fMAWy6AS4o2s5n5zSJj0M0GlJoyIWE", //keep this on the low :)
+  apiKey: FIREBASE_APIKEY,
   authDomain: "new-tab-2-d6042.firebaseapp.com",
   databaseURL: "https://new-tab-2-d6042-default-rtdb.firebaseio.com",
   projectId: "new-tab-2-d6042",
